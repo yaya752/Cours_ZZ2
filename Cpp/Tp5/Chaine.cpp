6d98e5fee01e19343501bac2f9c0f87006a89bde
yakhiara <yassine.khiara@etu.uca.fr>
2023-02-05 21:41:01 +0100

    tp suite

--- Cpp/Tp5/Chaine.cpp
+++ Cpp/Tp5/Chaine.cpp
@@ -5,12 +5,24 @@ Chaine::Chaine():capacite{-1},tab{nullptr}
 
 }
 Chaine::Chaine(const char* inCS):capacite{(signed) strlen(inCS) }
-{
-    tab = (char*)inCS;
+{   
+    tab = new char[capacite+1];
+    strcpy(tab, inCS);
     
 }
-Chaine::~Chaine(){
-
+Chaine::Chaine(const Chaine& c1):capacite{c1.capacite}
+{
+    tab = new char[capacite+1];
+    strcpy(tab, c1.tab);
+    std::cout << "Constructeur de copie de Chaine.\n";
+}
+Chaine::Chaine(int capa):capacite{capa},tab{new char[capa+1]}
+{
+    tab[0] ='\0';
+}
+Chaine::~Chaine()
+{
+    delete [] tab;
 }
 char* Chaine::c_str() const
 {
@@ -19,4 +31,20 @@ char* Chaine::c_str() const
 int Chaine::getCapacite() const
 {
     return capacite;
+}
+void Chaine::afficher() const 
+{
+
+}
+void Chaine::afficher(std::ostream &fr) const
+{
+    fr << tab;
+
+}
+Chaine& Chaine::operator=(const Chaine& x)
+{
+    tab = new char[x.capacite+1];
+    strcpy(tab, x.tab);
+    capacite = x.capacite;
+    return *this;
 }
--- Cpp/Tp5/Chaine.hpp
+++ Cpp/Tp5/Chaine.hpp
@@ -5,16 +5,23 @@
 #include <fstream>
 
 class Chaine {
-    int capacite;
-    char* tab;
+    public:
+        int capacite;
+        char* tab;
     
     public:
         Chaine();
         Chaine(const char* inCS);
+        Chaine(int capa);
+        Chaine( const Chaine& );           // Declare copy constructor.
+        Chaine& operator=(const Chaine& x);
         ~Chaine();
+        
     public:
         char* c_str() const;
         int getCapacite() const;
+        void afficher(std::ostream &) const;
+        void afficher() const;
         
 };
 
